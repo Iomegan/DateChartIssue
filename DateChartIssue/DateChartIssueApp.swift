34ad9f4436a0a6e3f55a54fb66dc4807c3a6fd2d
Iomegan <dwitt.kiel@icloud.com>
2022-09-10 12:17:30 +0200

    Initial Commit

---
+++ DateChartIssue/DateChartIssueApp.swift
@@ -0,0 +1,17 @@
+//
+//  DateChartIssueApp.swift
+//  DateChartIssue
+//
+//  Created by Daniel Witt on 10.09.22.
+//
+
+import SwiftUI
+
+@main
+struct DateChartIssueApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
